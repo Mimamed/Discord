e9b57582e313156695124346c495f2a3a68769b5
Bodi <mohamed474792@hotmail.com>
2020-04-06 02:39:02 +0200

    la till classer

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,7 +6,15 @@ public class MainClass
 
     public static void main (String[] args)
     {
+        setup();
+    }
 
+    private static void setup()
+    {
+        win.setVisible(true);
+        win.setSize(700, 500);
+        win.setLocationRelativeTo(null);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
