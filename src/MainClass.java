ae4b9cfc5f27accb75c1af5862fe32f6c3c4eb42
Bodi <mohamed474792@hotmail.com>
2020-04-17 13:19:48 +0200

    Bara en test med nätverk

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class MainClass
 {
@@ -8,6 +10,7 @@ public class MainClass
     static SendboxPanel SendBox = new SendboxPanel();
     static Panel[] paneler = {Chatt, SendBox, Contacts};
     static double xSize = 1, ySize = 1, size = 1;
+
 
     public static void main (String[] args)
     {
@@ -25,10 +28,24 @@ public class MainClass
         win.setVisible(true);
         win.setSize(717, 540);
         win.setLocationRelativeTo(null);
-        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        win.addWindowListener(new steng());
         /*win.setResizable(false);*/
 
         win.repaint();
+    }
+    private static void exit()
+    {
+        try
+        {
+            SendboxPanel.client.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        win.dispose();
+        System.exit(0);
     }
 
     public static void makeMessage(String text)
@@ -39,6 +56,39 @@ public class MainClass
         win.setSize(300,200);
         win.setLocationRelativeTo(null);
         win.setVisible(true);
+    }
+
+    static class steng implements WindowListener
+    {
+
+        public void windowOpened(WindowEvent e) {
+
+        }
+
+        public void windowClosing(WindowEvent e)
+        {
+            exit();
+        }
+
+        public void windowClosed(WindowEvent e) {
+
+        }
+
+        public void windowIconified(WindowEvent e) {
+
+        }
+
+        public void windowDeiconified(WindowEvent e) {
+
+        }
+
+        public void windowActivated(WindowEvent e) {
+
+        }
+
+        public void windowDeactivated(WindowEvent e) {
+
+        }
     }
 
 }
